6595d41b9a34ce145f73e913f620a3025958d2e4
cevinthomas <Nygiants1>
2022-04-18 16:04:44 +0200

    categories

--- src/Categories/repository.ts
+++ src/Categories/repository.ts
@@ -1,5 +1,5 @@
-import { CreateCategoryPayload } from "./interfaces";
-import db, { sql } from "../../db/database";
+import {CreateCategoryPayload} from "./interfaces";
+import db, {sql} from "../../db/database";
 
 export const createCategoryRep = async (
   createPayload: CreateCategoryPayload
@@ -13,15 +13,38 @@ export const createCategoryRep = async (
 
 export const fetchAllCategoriesRep = async () => {
   try {
-    await db.query(sql`
+    return ( await db.query(sql`
     SELECT categories.title, array_agg(tags.title) 
       FROM categories
       LEFT JOIN tags on categories.category_id = tags.category_id
       GROUP BY categories.title
-`);
+`);)
+
   } catch (e) {}
 };
 
-export const fetchCategoryRep = (categoryId: number) => {};
+export const fetchCategoryRep = (categoryId: number) => {
+  try {
+   await db.query(sql`
+        SELECT categories.title, array_agg(tags.title)
+        FROM categories
+        LEFT JOIN tags on categories.category_id = tags.category_id
+        WHERE tags.category_id = ${categoryId}
+          GROUP BY categories.title
+`)
+  } catch (e) {
 
-export const deleteCategoryRep = (categoryId: number) => {};
+  }
+};
+
+export const deleteCategoryRep = (categoryId: number) => {
+  try {
+ await db.query(sql`
+        DELETE FROM categories 
+          WHERE category_id = ${categoryId}
+`)
+  } catch (e) {
+
+  }
+
+};
