808f1965bbf5ce34a374f6bd00403d36814b2204
cevinthomas <Nygiants1>
2022-05-21 12:11:33 +0200

    move

---
+++ client/dist/App.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var App = function () {
+    return ();
+};
+exports.default = App;
+//# sourceMappingURL=App.js.map
